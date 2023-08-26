60fcb20291ec0fe5a50f5e6ea91b95458f7c2b37
gennhonglin <73550732+gennhonglin@users.noreply.github.com>
2023-08-25 23:30:16 -0700

    implemented prosperity section when prosperity value is updated it also updates in the database as well as displaying the updated value on screen in the front end as well allowing the user to post and get from the database

--- controller/partyController.js
+++ controller/partyController.js
@@ -68,3 +68,30 @@ exports.loginParty = async (req, res) => {
     }
 }
 
+exports.updateProsperity = async (req, res) => {
+    try {
+        await knex('party').where({party_id: req.body.party_id}).update({prosperity_points: req.body.prosperity_points});
+        res.status(204).send(`party with id: ${req.body.party_id} prosperity points has been updated`);
+
+
+    } catch (err) {
+        res.status(400).send(`Error updating party prosperity ${err}`);
+    }
+}
+
+exports.displayProsperity = async (req, res) => {
+    try {
+        const data = await knex('party').where({ party_id: req.params.id });
+
+        if(!data.length) {
+            return res.status(404).send(`Player with id: ${req.params.id} is not a valid player`);
+        } else {
+            res.status(200).json(data[0].prosperity_points);
+        }
+
+
+    } catch (err) {
+        res.status(400).send(`Error retrieving Player: ${err}`);
+    }
+}
+
--- routes/partyRoute.js
+++ routes/partyRoute.js
@@ -12,6 +12,11 @@ router
     .route('/party/login')
         .post(partyController.loginParty);
 
+router
+    .route('/party/:id')
+        .get(partyController.displayProsperity)
+        .post(partyController.updateProsperity);
+
 
 
 module.exports = router;
